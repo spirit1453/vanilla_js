02a388623c6e64de008ef3585d95ec2d14437135
spirit <catchspirit37@gmail.com>
2018-09-10 16:49:38 +0800

    z

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable global-require */
 const result = {
   DateUtil: require('./util/DateUtil'),
-  StringUtil: require('./util/StringUtil')
+  StringUtil: require('./util/StringUtil'),
+  CliUtil: require('./util/CliUtil')
 }
 
 Object.freeze(result)

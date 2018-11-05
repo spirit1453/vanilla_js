17fcd4acc1588830d36aa843c55aeaa474a04c87
spirit <catchspirit37@gmail.com>
2018-11-05 10:30:05 +0800

    z - Mon Nov 05 2018 10:30:05 GMT+0800 (CST)

--- src/util/StringUtil.js
+++ src/util/StringUtil.js
@@ -3,6 +3,16 @@ class Cls {
   static eqIgnoreCase (str1, str2) {
     return str1.toLowerCase() === str2.toLowerCase()
   }
+
+  static stripNewline (str, option = {}) {
+    const {shouldTrim = true, replaceStr = ' '} = option
+    let result = str
+    if (shouldTrim) {
+      result = result.trim()
+    }
+    result = result.replace(/\n/g, replaceStr)
+    return result
+  }
 }
 
 Object.freeze(Cls)

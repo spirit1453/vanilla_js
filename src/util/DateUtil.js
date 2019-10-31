25ccf5f937983e015c57d49ca6a29229861d2252
catch <catchspirit37@gmail.com>
2019-10-31 10:11:06 +0800

    z - Thu Oct 31 2019 10:11:05 GMT+0800 (China Standard Time)

--- src/util/DateUtil.js
+++ src/util/DateUtil.js
@@ -1,12 +1,15 @@
+const moment = require('moment')
 
 class Cls {
   static paddingDate (date) {
     date = date.toString()
     return date.padStart(2, '0')
   }
-  static getTimeDisplay () {
-    const date = new Date()
-    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+  static getTimeDisplay (date) {
+    date = date || new Date()
+
+    const result = moment(date).format('YYYY-MM-DD_HH:mm')
+    return result
   }
 }
 

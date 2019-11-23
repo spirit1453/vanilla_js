c9fe2a591a28ee976c69cb96cb8a3b3f1d038155
catch <catchspirit37@gmail.com>
2019-11-23 17:16:45 +0800

    z - Sat Nov 23 2019 17:16:45 GMT+0800 (China Standard Time)

--- src/util/FileUtil.js
+++ src/util/FileUtil.js
@@ -3,6 +3,18 @@ class Cls {
   static removeExt (fileName) {
     return fileName.split('.')[0]
   }
+  static parseFileName(fileName) {
+    const ary = fileName.split('.')
+    const extension = ary[1]
+    const pureName = ary[0]
+
+    const result = {
+      extension,
+      pureName
+    }
+
+    return result
+  }
 }
 
 Object.freeze(Cls)
